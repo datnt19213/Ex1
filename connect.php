84a6a6bd3544ac4985310ca6535457cbc4a57f71
datnt19213 <91647943+datnt19213@users.noreply.github.com>
2021-10-23 09:16:43 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,4 +1,6 @@
-$con = pg_connect("postgres://vazfilvivcifkk:036fa69284856775313ca6d56d279c984e2fd7367e2359e2692f3fa36e28955a@ec2-18-234-15-247.compute-1.amazonaws.com:5432/d4iifhmlvp4cgb");
-if(!$con){
-    echo die("ERROR CONNECT DB");
-}
+<?php
+    $con = pg_connect("postgres://vazfilvivcifkk:036fa69284856775313ca6d56d279c984e2fd7367e2359e2692f3fa36e28955a@ec2-18-234-15-247.compute-1.amazonaws.com:5432/d4iifhmlvp4cgb");
+    if(!$con){
+        echo die("ERROR CONNECT DB");
+    }
+?>
